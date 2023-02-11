c977beeb65103d0532d06a25fafb1e427193d58c
fad-anes <anes.fad@esprit.tn>
2023-02-11 21:49:12 +0100

    add function insert delete readall

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,23 @@
 import utils.Datasource;
-
+import entite.Personne;
+import service.servicepersonne;
 import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args)  {
         Datasource ds1= new Datasource().getInstance();
         System.out.println(ds1);
+        Personne p=new Personne("3a9", "test", 23);
+        Personne p1=new Personne("3a9", "test1", 20);
+        Personne p2=new Personne(5,"3", "0", 2);
+        servicepersonne ps=new servicepersonne();
+       // ps.insert(p);
+       // ps.insert(p1);
+       // ps.insert(p2);
+        //ps.readall().forEach(System.out::println);
+        //ps.delete(p2);
+        //ps.readall().forEach(System.out::println);
+        ps.update(p2);
 
     }
 }
--- src/service/servicepersonne.java
+++ src/service/servicepersonne.java
@@ -4,6 +4,10 @@ import entite.Personne;
 import java.util.List;
 import utils.Datasource;
 import java.sql.Connection;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class servicepersonne implements Iservice<Personne>{
     private Connection conn;
@@ -13,27 +17,69 @@ public class servicepersonne implements Iservice<Personne>{
 
 
     @Override
-    public void insert(Personne personne) {
-
+    public void insert(Personne t) {
+        String requete = "insert into personne (nom,prenom,age) values "
+                + "('" + t.getNom() + "','" + t.getPrenom() + "'," + t.getAge() + ")";
+        try {
+            Statement st = conn.createStatement();
+            st.executeUpdate(requete);
+        } catch (SQLException ex) {
+            Logger.getLogger(servicepersonne.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     @Override
-    public void delete(Personne personne) {
+    public void delete(Personne t) {
+        String requete3 = "DELETE FROM personne WHERE id=" + t.getId();
+        try {
+            Statement st = conn.createStatement();
+            st.execute(requete3);
+            System.out.println(" personne  supprimé !");
 
+        } catch (SQLException ex) {
+            Logger.getLogger(servicepersonne.class.getName()).log(Level.SEVERE, null, ex);
+
+        }
     }
 
     @Override
-    public void update(Personne personne) {
-
+    public void update(Personne t) {
+        String requete2 =
+        " UPDATE personne SET " + " nom = ?,prenom=?," +
+                " age = ?  WHERE id= " + t.getId();
+        try {
+            PreparedStatement pst = conn.prepareStatement(requete2);
+            pst.setString(1, t.getNom());
+            pst.setString(2, t.getPrenom());
+            pst.setInt(3, t.getAge());
+            pst.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(servicepersonne.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     @Override
     public List<Personne> readall() {
-        return null;
+        List<Personne> list=new ArrayList<>();
+        String requete="select * from personne";
+        try {
+            Statement st=conn.createStatement();
+            ResultSet rs=st.executeQuery(requete);
+            while(rs.next()){
+                Personne p=new Personne(rs.getInt("id"), rs.getString(2),
+                        rs.getString("prenom"), rs.getInt("age"));
+                list.add(p);
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(servicepersonne.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return list;
     }
 
     @Override
     public Personne ReadById(int id) {
+
         return null;
     }
 }
